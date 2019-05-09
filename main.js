e30fd98619d78022fd5d793aec49a9229fa7d4b2
dakotachasesmith <dakotachasesmith@gmail.com>
2019-05-09 02:16:03 -1000

    Main.js:format - Enforce return type -> String, fix problem with formatting the entirety of floating point error

--- main.js
+++ main.js
@@ -314,39 +314,23 @@ $("#battle").click(() => {
 });
 
 function format(number, decPlaces = 2) {
-    // 2 decimal places => 100, 3 => 1000, etc
-    decPlaces = Math.pow(10, decPlaces);
+    // Abbreviations to enumerate over
+    const abbrev = ["", "K", "M", "B", "T", "q", "Q", "s", "S", "O", "N", "D"];
+    let exponent = Math.max(0, Math.log10(number));
+    let suffixIdx = Math.floor(exponent / 3);
+    number = number / Math.pow(10, Math.floor(exponent / 3) * 3);
 
-    // Enumerate number abbreviations
-    var abbrev = ["K", "M", "B", "T", "q", "Q", "s", "S", "O", "N", "D"];
-
-    // Go through the array backwards, so we do the largest first
-    for (var i = abbrev.length - 1; i >= 0; i--) {
-
-        // Convert array index to "1000", "1000000", etc
-        var size = Math.pow(10, (i + 1) * 3);
-
-        // If the number is bigger or equal do the abbreviation
-        if (size <= number) {
-            // Here, we multiply by decPlaces, round, and then divide by decPlaces.
-            // This gives us nice rounding to a particular decimal place.
-            number = Math.round(number * decPlaces / size) / decPlaces;
-
-            // Handle special case where we round up to the next abbreviation
-            if ((number == 1000) && (i < abbrev.length - 1)) {
-                number = 1;
-                i++;
-            }
-
-            // Add the letter for the abbreviation
-            number += abbrev[i];
-
-            // We are done... stop
-            break;
-        }
+    // Handle special case where we round up to the next abbreviation
+    if ((Math.round(number) === 1000) && (exponent / 3 < abbrev.length - 1)) {
+        number = 1;
+        ++suffixIdx;
     }
 
-    return number;
+    // Return the string with a decimal place if decimal portion is nonzero
+    if (number == Math.floor(number))
+        return `${number}${abbrev[suffixIdx]}`;
+    else
+        return `${number.toFixed(decPlaces)}${abbrev[suffixIdx]}`;
 }
 
 var updateId = setInterval(() => {
